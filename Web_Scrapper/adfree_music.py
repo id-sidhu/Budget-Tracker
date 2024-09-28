1952d156b15a0e5e1b7a71f02ed81cd23fc153b8
Harinder <hsidhu2606@gmail.com>
2024-09-28 01:22:14 -0400

    Added funtionality to display song name.

--- Web_Scrapper/adfree_music.py
+++ Web_Scrapper/adfree_music.py
@@ -4,21 +4,39 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from pytube import YouTube
+import os
 
-brave_path = "/opt/brave.com/brave/brave"
-chromedriver_path = "/usr/local/bin/chromedriver"
+def now_playing():
+    new_video = driver.current_url
+    if new_video != current_video:
+        clear_terminal()
+        print(f"Now playing: {YouTube(new_video).title}")
+    else:
+        print(f"Now playing: {YouTube(current_video).title}")
 
-options = webdriver.ChromeOptions()
-options.binary_location = brave_path
-
-driver = webdriver.Chrome(service=Service(chromedriver_path), options=options)
-
+def clear_terminal():
+    if os.name() == "nt":
+        os.system('cls')
+    else:
+        os.system('clear')
 try:
+    brave_path = "/opt/brave.com/brave/brave"
+    chromedriver_path = "/usr/local/bin/chromedriver"
+
+    options = webdriver.ChromeOptions()
+    options.binary_location = brave_path
+
+    driver = webdriver.Chrome(service=Service(chromedriver_path), options=options)
+
+
     search_query = (input("Enter the title of song or singer name: "))
+    if search_query == "":
+        search_query = "latest songs"
+
     driver.get("https://youtube.com")
 
     search_box = driver.find_element('name', 'search_query')
-
     search_box.send_keys(search_query)
     search_box.send_keys(Keys.RETURN)
 
@@ -26,6 +44,11 @@ try:
         EC.presence_of_element_located((By.XPATH, '(//a[@id="video-title"])[1]'))
     )
     first_video.click()
-    input("Press any key to quit the browser...")
+
+    current_video = driver.current_url
+
+    now_playing()
+
+    input("Press any key to quit...")
 finally:
     driver.quit()
